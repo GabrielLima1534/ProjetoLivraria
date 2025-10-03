672518dbc483b731e7ccd2eede558d0f14655405
2DM <gbrlima1534@gmail.com>
2025-10-03 10:18:42 -0300

    final

--- src/Livraria/Main.java
+++ src/Livraria/Main.java
@@ -6,23 +6,28 @@ public class Main {
 		
 	Biblioteca biblioteca = new Biblioteca();
 	
-//	Professor 1 = new Professor();
+    Usuario P1 = new Professor(3, "Mario", "mariosgames@gmail.com");
 //	Professor 2 = new Professor();
 //	Professor 3 = new Professor();
+    
+    biblioteca.adicionarUsuario(P1);
 	
-	Usuario u = new Aluno(1, "Gabriel", "gabriel@gmail.com");
-//	Aluno 2 = new Aluno();
+	Usuario Aluno1 = new Aluno(1, "Gabriel", "gabriel@gmail.com");
+	Usuario Aluno2 = new Aluno(2, "João", "Joao@gmail.com");
 //	Aluno 3 = new Aluno();
 	
-	biblioteca.adicionarUsuario(u);
+	biblioteca.adicionarUsuario(Aluno1);
+	biblioteca.adicionarUsuario(Aluno2);
 	
-	Livro l = new Livro("1","dom casmurro", "Machado de Assis");
-//	Livro 2 = new Livro();
-//	Livro 3 = new Livro();
-	
-	biblioteca.adicionarLivro(l);
+	Livro A = new Livro("1","dom casmurro", "Machado de Assis");
+	Livro B = new Livro("2", "Harry Potter", "j.k rolling");
+    Livro C = new Livro("3", "Mangá Naruto", "Masashi Kishimoto");
+	Livro D = new Livro("4", "Mangá One Piece", "Eiichiro Oda");
+	biblioteca.adicionarLivro(A);
+	biblioteca.adicionarLivro(B);
 	
 	biblioteca.emprestarLivro(1,"1");
+	biblioteca.emprestarLivro(3, );
 
 
 	
--- src/Livraria/Professor.java
+++ src/Livraria/Professor.java
@@ -2,6 +2,16 @@ package Livraria;
 
 public class Professor extends Usuario {
 	
+	public Professor(){
+		
+	}
+	
+	public Professor(int id, String nome, String email) {
+		super(id, nome, email);
+	}
+	
+	
+
 	public int getLimiteEmprestimos() {
 		return 5;
 	}
